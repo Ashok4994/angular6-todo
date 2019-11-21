9a37188052aa77fb1c51a9c2b748e06c1a8f73f5
Ashok4994 <thota.ashokchakravarthy@gmail.com>
2019-11-20 22:48:25 -0800

    first commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
+})
+export class TodoComponent implements OnInit {
+toDoListArray: any[];
+
+  constructor(private toDoService: TodoService) { }
+
+  ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x['$key'] = element.key;
+        this.toDoListArray.push(x);
+      });
+
+      this.toDoListArray.sort((a, b) => {
+        return a.isChecked - b.isChecked;
+      });
+
+
+    }
+
+    );
+  }
+
+  OnAdd(itemtitle) {
+    this.toDoService.addTitle(itemtitle.value);
+    itemtitle.value = null;
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyBtHO8G5JpOez6sM-5J_L2oo1RB-IrPXX4',
+    authDomain: 'todolist-9f0c3.firebaseapp.com',
+    databaseURL: 'https://todolist-9f0c3.firebaseio.com',
+    projectId: 'todolist-9f0c3',
+    storageBucket: 'todolist-9f0c3.appspot.com',
+    messagingSenderId: '503890646344',
+    appId: '1:503890646344:web:8e19b851bbe7bee46a8f7d',
+    measurementId: 'G-RN2T2B93KR'
+  }
 };
