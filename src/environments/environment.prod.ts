e6f2a287c250a0b63a3535e40f59c5c76f6389c9
Ashok4994 <thota.ashokchakravarthy@gmail.com>
2019-11-22 10:48:45 -0800

    Modify server prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBtHO8G5JpOez6sM-5J_L2oo1RB-IrPXX4',
+    authDomain: 'todolist-9f0c3.firebaseapp.com',
+    databaseURL: 'https://todolist-9f0c3.firebaseio.com',
+    projectId: 'todolist-9f0c3',
+    storageBucket: 'todolist-9f0c3.appspot.com',
+    messagingSenderId: '503890646344',
+    appId: '1:503890646344:web:8e19b851bbe7bee46a8f7d',
+    measurementId: 'G-RN2T2B93KR'
+  }
 };
